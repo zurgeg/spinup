05b844230b3754f3f37782c17173c1da18b9b784
zurgeg <zurgeg@gmail.com>
2021-03-29 15:47:05 -0400

    Remove firmware to save space

--- boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-qemu-sytem-x86-64 \
-	-machine q35,kernel_irq_chip=on,accel=kvm \
-	-cpu Skylake-Client,kvm=off,+sse2,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
-	-smp 8,cores=4 \
-	-device ich9-ahci,id=sata \
-	-drive id=drive0,if=none,format=qcow2,file=testing.qcow2 \
-	-device ide-hd,bus=sata.0,drive=drive0 \
-	-drive if=pflash,format=raw,readonly,file="firmware/OVMF.fd" \
